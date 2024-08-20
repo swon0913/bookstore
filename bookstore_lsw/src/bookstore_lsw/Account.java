97af99c0512d7f2df0cb340d898352e3029e727b
Seongwon <Seongwon@DESKTOP-GPN6H4M>
2024-08-21 02:11:25 +0900

    Update

--- bookstore_lsw/src/bookstore_lsw/Account.java
+++ bookstore_lsw/src/bookstore_lsw/Account.java
@@ -65,7 +65,9 @@ public class Account {
 
 		System.out.print("비밀번호를 입력하세요: ");
 		String password = scanner.nextLine();
-
+		
+		Account LoginAccount =authenticate(id,password);
+		
 		/*
 		 * 사용자의 ID, PW를 Scanner 로 입력 받음 아래 authenticate() 함수를 호출하여 회원정보가 일치하는지 확인 회원 정보가
 		 * 존재하는 경우, 해당 유저의 데이터를 반환함 없는 경우 null 반환
@@ -76,21 +78,38 @@ public class Account {
 
 	// 로그인 인증
 	private static Account authenticate(String id, String password) {
+		List<Account> accounts = getAllAccounts();
+		
+		for(Account account : accounts) {
+			if(account.getId().equals(id)&& account.getPassword().equals(password)) {
+				return account;
+			}
+		}
 		/*
 		 * getAllAccounts() 함수를 호출하여 모든 사용자의 데이터를 가져옴 for-each 문으로 가져온 정보와 맞는지 확인함 맞는 경우
 		 * 해당 데이터(for-each 요소)를 반환함 없는 경우 null 반환
 		 */
-		Account null_data = null;
-		return null_data;
+		
+		return null;
 	}
 
 	// 관리자용 : 모든 회원 정보 반환
 	public static List<Account> getAllAccounts() {
+		List<String> accountAllData = DBUtil.read("account");
+		List<Account> accounts = new ArrayList<>();
+		
+		for(String data : accountAllData) {
+			String[] fields = data.split(",");
+			int index = Integer.parseInt(fields[0]);
+			String name = fields[1];
+			String id = fields[2];
+			String password = fields[3];
+			accounts.add(new Account(index, name,id,password)); //내일 한번체크
+		}
 		/*
 		 * 모든 회원 정보를 리스트에 담아서 반환
 		 */
-		List<Account> null_data = null;
-		return null_data;
+		return accounts;
 	}
 
 	// 회원가입용 : 가장 마지막 회원 번호 +1 반환
